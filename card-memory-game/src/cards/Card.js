2813a8b37a2c919bcb2ab45975b9b57a892ad3fa
francoandreDev <francoandre.dev@gmail.com>
2022-09-21 23:20:38 -0500

    card-memory-game

---
+++ card-memory-game/src/cards/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Card.css';
+
+const Card = ({status}) => {
+    return (
+        <div className={`Card ${status}`}>
+
+        </div>
+    );
+};
+
+export default Card;
